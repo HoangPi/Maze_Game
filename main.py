ea1ede0d7e8eeda1db5153f59ad84a69614852c3
Phi Vo <voduchoangphi@gmail.com>
2023-12-12 13:24:32 +0700

    Near final commit

--- main.py
+++ main.py
@@ -32,12 +32,25 @@ cv2.namedWindow('maze')
 cv2.createTrackbar('C', 'maze', 10, 100, nothing)
 # cv2.createButton("maze",back,None,cv2.QT_PUSH_BUTTON,1)
 cv2.setTrackbarPos('C','maze',10)
-cv2.imshow('maze',truemaze)
+text = 'Press 1:BFS, 2:DFS, 3:A*, 4:UCS, 5:Greedy'
+org = (50, 50)  # bottom-left corner of the text string in the image
+font = cv2.FONT_HERSHEY_SIMPLEX
+fontScale = 1
+color = (255, 0, 0)  # BGR tuple for blue color
+
+t = cv2.cvtColor(truemaze,cv2.COLOR_GRAY2BGR)
+cv2.imshow('maze',cv2.putText(t,text,(0,590),font,0.5,color,1,cv2.LINE_AA))
+
+
+
 while(True):
     if(defaultvalue!=cv2.getTrackbarPos('C','maze') and cv2.getTrackbarPos('C','maze')!=0):
         defaultvalue=cv2.getTrackbarPos('C','maze')
         truemaze, grid=adjustMaze(d=defaultvalue)
-        cv2.imshow('maze',truemaze)
+        
+        # cv2.imshow('maze',truemaze)
+        t = cv2.cvtColor(truemaze,cv2.COLOR_GRAY2BGR)
+        cv2.imshow('maze',cv2.putText(t,text,(0,590),font,0.5,color,1,cv2.LINE_AA))
 
     k = cv2.waitKey(5) & 0xFF
     
